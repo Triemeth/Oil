14782ea06452c718304cab04d3adea2a434a56e8
Ethan Triem <EJTriem@outlook.com>
2025-09-10 17:39:13 -0500

    converted oil price to json may not be needed

--- Early_test_scripts/oil_price_to_json.py
+++ Early_test_scripts/oil_price_to_json.py
@@ -2,11 +2,23 @@ import pandas as pd
 import json
 
 if __name__ == "__main__":
+    
     oil_price = pd.read_csv("../Early_data_test/DCOILBRENTEU.csv")
-    oil_price_dict = oil_price.to_dict(orient='index')
+
+    dat = { "oil_prices": []}
+
+    print(oil_price.tail())
+    print(oil_price.head())
+
+    for i in range(0, len(oil_price)):
+        row = {
+            "observation_date": oil_price["observation_date"].iloc[i],
+            "Price": oil_price["DCOILBRENTEU"].iloc[i]
+        }
+
+        dat["oil_prices"].append(row)
 
     with open("../Early_data_test/oil_price.json", "w") as json_file:
-        json.dump(oil_price_dict, json_file, indent=4)
-
+        json.dump(dat, json_file, indent=4)
 
 #py -3.12 oil_price_to_json.py
